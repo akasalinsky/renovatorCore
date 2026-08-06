3ff7457b14e42fccba285bcc8a0030db7d6a3602
akasalinsky <akasalinsky@gmail.com>
2026-08-06 17:01:05 +0400

    v.1.0 moshno publicovat

---
+++ src/main/java/org/example/service/money/PriceCatalog.java
@@ -0,0 +1,4 @@
+package org.example.service.money;
+
+public class PriceCatalog {
+}
---
+++ src/test/java/org/example/service/RoomBudgetServiceTest.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Тесты проверки расчета бюджета ремонта")
+public class RoomBudgetServiceTest {
+}
